6a557f5cd43f1ecf76b15f389bc03e81bb9cb788
Adfinitas Tech1 <adfinitech1@dhcp112.adfinitas.fr>
2017-10-02 15:20:40 +0200

    Envoie du formulaire via AJAX, affichage du message paiement accepté/refusé sans reload la page

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 var stripe = require("stripe")("sk_test_laxl5BP0TNodFtPrFaBsrKZm");
 var express = require('express');
 var app = express();
-var request = require('request');
+//var request = require('request');
 var bodyParser = require('body-parser');
-var urlEncoded = bodyParser.urlencoded({ extended: false });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//var urlEncoded = bodyParser.urlencoded({ extended: true });
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://kxtlvtghnbpyvx:55de3be945b0c2abfb037eb79147c7df0981c260b2a8b80836a4efab12db6c25@ec2-54-75-227-173.eu-west-1.compute.amazonaws.com:5432/ded0orheosck9p');
-//const sequelize = new Sequelize('postgres://postgres:satinifda@localhost:5432/paiement');
+//const sequelize = new Sequelize('postgres://kxtlvtghnbpyvx:55de3be945b0c2abfb037eb79147c7df0981c260b2a8b80836a4efab12db6c25@ec2-54-75-227-173.eu-west-1.compute.amazonaws.com:5432/ded0orheosck9p');
+const sequelize = new Sequelize('postgres://postgres:satinifda@localhost:5432/paiement');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 var Regex = require('regex');
 
 var debug = function(obj, objname)
 {
     console.log('\n--------\n\n' + objname + ' object: ');
     console.log(JSON.stringify(obj, null, 4));
-    //for (i in obj){console.log(i + ' => ' + obj[i]);}
+    for (i in obj){console.log(i + ' => ' + obj[i]);}
 };
 
 var addTransaction = function(civilite, nom, prenom, montant, type, email, tel, adresse) {
@@ -140,24 +144,15 @@ var checkError = function (data) {
     return (0);
 };
 
+server.listen(process.env.PORT || 8080);
+app.io = io;
 app.use(express.static(__dirname + '/public'))
 
     .get('/', function (req, res) {
         res.end('bonjour');
     })
 
-    .get('/accepte', function (req, res) {
-
-        res.header("Content-Type", "text/html;charset=utf-8");
-        res.end('Paiement <span style="color: green;">accepté</span> !');
-    })
-
-    .get('/refuse', function (req, res) {
-        res.header("Content-Type", "text/html;charset=utf-8");
-        res.end('Paiement <span style="color: red;">refusé</span> !');
-    })
-
-    .post('/', urlEncoded, function (req, res) {
+    .post('/', function (req, res) {
         debug(req.body, 'req.body');
         if (checkError(req.body) === 84) {
             console.log('Refused by server.');
@@ -165,66 +160,6 @@ app.use(express.static(__dirname + '/public'))
             return (84);
         }
         else if (req.body.paymentType === 'creditCard') {
-            // console.log('QUERY CODE : ' + req.query.code);
-            // return stripe.accounts.create({
-            //     country: 'FR',
-            //     type: 'custom',
-            //     email: req.body.email,
-            //     legal_entity : {
-            //         first_name: req.body.prenom,
-            //         last_name: req.body.nom,
-            //         type: 'individual',
-            //     },
-            // }).then(function (account) {
-            //     debug(account, 'account');
-            //     return stripe.charges.create({
-            //         amount: parseInt(req.body.amount * 100),
-            //         currency: 'eur',
-            //         source: 'tok_visa',
-            //     }, {
-            //         stripe_account: account.id,
-            //     })
-            // }).then(function (charge) {
-            //     debug(charge, 'charge');
-            //     res.redirect('/accepte');
-            // }).catch(function (error) {
-            //     debug(error, 'error');
-            //     res.redirect('/refuse');
-            // });
-            // request.post({
-            //     url: 'https://connect.stripe.com/oauth/token',
-            //     form: {
-            //         grant_type: 'authorization_code',
-            //         client_id: 'ca_BNbQ2Q1Lml6ynQtmAckLjRZzvHtW9mkq',
-            //         code: req.query.code,
-            //         client_secret: 'sk_test_laxl5BP0TNodFtPrFaBsrKZm'
-            //     }
-            // }, function(err, r, response) {
-            //     var accessToken = JSON.parse(response).access_token;
-            //     console.log('ACCESS TOKEN:' + accessToken);
-            //     debug(response, 'reponse');
-            //     stripe.charges.create({
-            //         amount: parseInt(req.body.amount * 100),
-            //         currency: 'eur',
-            //         source: response.stripe_user_id,
-            //     }).then(function(charge) {
-            //         // asynchronously called
-            //         debug(charge, 'charge')
-            //         res.redirect('/accepte');
-            //     });
-            // });
-
-
-
-            // stripe.accounts.create({
-            //     country: "FR",
-            //     type: "custom",
-            //     email: 'tech@adfinitas.fr',
-            //
-            // }).then(function(acct) {
-            //     debug(acct, 'account');
-            //     // asynchronously called
-            // });
             return stripe.charges.create({
                 source: req.body.stripeSource,
                 amount: parseInt(req.body.amount * 100),
@@ -234,12 +169,79 @@ app.use(express.static(__dirname + '/public'))
                     addTransaction(req.body.civilite, req.body.nom, req.body.prenom, req.body.amount, 'creditCard', req.body.email, req.body.tel, makeAdress(req.body))
                 })
                 .then(function () {
-                    console.log('terminé');
-                    res.redirect('/accepte');
+                    req.app.io.emit('paymentSuccess');
                 })
                 .catch(function (err) {
                     debug(err, 'Error');
-                    res.redirect('/refuse');
+                    req.app.io.emit('paymentError');
+                    // message = '';
+                    // switch (err.type) {
+                    //     case 'StripeCardError':
+                    //         // A declined card error // => e.g. "Your card's expiration year is invalid."
+                    //         switch (err.code) {
+                    //             case 'invalid_number':
+                    //                 message = 'Le numéro de carte n\'est pas un numéro de carte de crédit valide.';
+                    //                 break;
+                    //             case 'invalid_expiry_month':
+                    //                 message = 'Le mois de la date d\'expiration de la carte n\'est pas valide.';
+                    //                 break;
+                    //             case 'invalid_expiry_year':
+                    //                 message = 'L\'année de la date d\'expiration de la carte n\'est pas valide.';
+                    //                 break;
+                    //             case 'invalid_cvc':
+                    //                 message = 'Le code de sécurité de la carte n\'est pas valide.';
+                    //                 break;
+                    //             case 'invalid_swipe_data':
+                    //                 message = 'Le swipe de votre carte n\'est pas valide.';
+                    //                 break;
+                    //             case 'incorrect_number':
+                    //                 message = 'Le numéro de carte est incorrecte.';
+                    //                 break;
+                    //             case 'expired_card':
+                    //                 message = 'La carte de crédit à expiré';
+                    //                 break;
+                    //             case 'incorrect_cvc':
+                    //                 message = 'Le code de sécurité de la carte est incorrecte.';
+                    //                 break;
+                    //             case 'incorrect_zip':
+                    //                 message = 'Le code zip de la carte est incorrecte.';
+                    //                 break;
+                    //             case 'card_declined':
+                    //                 message = 'La carte de crédit à été refusé.';
+                    //                 break;
+                    //             case 'missing':
+                    //                 message = 'Il n\'y à pas de carte de crédit sur le client chargé';
+                    //                 break;
+                    //             case 'processing_error':
+                    //                 message = 'Une erreur est survenue pendant le traitement de la carte';
+                    //                 break;
+                    //         }
+                    //         break;
+                    //     case 'RateLimitError':
+                    //         // Too many requests made to the API too quickly
+                    //         message = 'Trop de requêtes ont été envoyés en trop peu de temps.';
+                    //         break;
+                    //     case 'StripeInvalidRequestError':
+                    //         // Invalid parameters were supplied to Stripe's API
+                    //         message = '';
+                    //         break;
+                    //     case 'StripeAPIError':
+                    //         // An error occurred internally with Stripe's API
+                    //         message = '';
+                    //         break;
+                    //     case 'StripeConnectionError':
+                    //         // Some kind of error occurred during the HTTPS communication
+                    //         message = '';
+                    //         break;
+                    //     case 'StripeAuthenticationError':
+                    //         // You probably used an incorrect API key
+                    //         message = '';
+                    //         break;
+                    //     default:
+                    //         // Handle any other types of unexpected errors
+                    //         message = '';
+                    //         break;
+                    // }
                 });
         }
         else if (req.body.paymentType === 'sepa') {
@@ -259,14 +261,15 @@ app.use(express.static(__dirname + '/public'))
                 })
                 .then(function () {
                     addTransaction(req.body.civilite, req.body.nom, req.body.prenom, req.body.amount, 'sepa', req.body.email, req.body.tel, makeAdress(req.body));
-                    console.log('Paiment sepa réussi !');
-                    res.redirect('/accepte');
+                    req.app.io.emit('paymentSuccess');
                 })
                 .catch(function (err) {
                     debug(err, 'Error');
-                    res.redirect('/refuse');
+                    req.app.io.emit('paymentError');
                 });
         }
-    })
+    });
 
-.listen(process.env.PORT || 8080);
+
+
+//.listen(process.env.PORT || 8080);
